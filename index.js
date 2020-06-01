dc77a5f89300102a6bdcd3c1cf7d02a31a0c14a7
Lungaville <lordfrozt104@gmail.com>
2020-06-02 00:48:15 +0700

    Coba heroku

--- index.js
+++ index.js
@@ -50,12 +50,14 @@ app.post("/login", async function (req, res) {
     console.log(req.body);
     const conn = await db.getConnection()
     try {
-        const result = await db.executeQuery(conn, `select * from user where username = '${req.body.username}'and password= '${req.body.password}' )`)
+        const result = await db.executeQuery(conn, `select * from user where username = '${req.body.username}' and password= '${req.body.password}' `);
         if (result.length > 0) {
             let user = result[0]
             delete user.password;
-            var token = jwt.sign(user, process.env.SECRET_JWT);
-            const updateToken = await db.executeQuery(conn, `update user set token = ${token} where id= ${result.id} )`)
+            var token = jwt.sign({user : user}, process.env.SECRET_JWT);
+            console.log(token);
+            const updateToken = await db.executeQuery(conn, `update user set token = '${token}' where id= ${user.id} `)
+            // console.log()
             if (updateToken) {
                 user.token = token;
                 conn.release();
@@ -67,6 +69,88 @@ app.post("/login", async function (req, res) {
         return res.json(error)
     }
 })
+
+
+app.post('/products',async function(req,res){
+     const conn = await db.getConnection()
+     try {
+          const result = await db.executeQuery(conn,`insert into product values(NULL,'${req.body.name}')`)
+          const data = await db.executeQuery(conn,`select * from product where id = ${result.insertId}`)
+          conn.release()
+          if(result && data.length >0){
+               return res.json({message : 'Success Insert', data : data[0]}) 
+         }
+    } catch (error) {
+        conn.release()
+        return res.json(error)
+}
+});
+app.get('/products',async function(req,res){
+     const conn = await db.getConnection()
+     try {
+          const result = await db.executeQuery(conn,`select * from product`)
+          conn.release()
+          if(result){
+               return res.json(result) 
+         }
+    } catch (error) {
+        conn.release()
+        return res.json(error)
+}
+});
+app.get('/products/:id',async function(req,res){
+     const conn = await db.getConnection()
+     try {
+          const result = await db.executeQuery(conn,`select * from product where id=${req.params.id}`)
+          conn.release()
+          if(result.length>0){
+               return res.json(result) 
+         }
+          else {
+               return res.status(404).json({message : 'Data tidak ditemukan'})
+}
+    } catch (error) {
+        conn.release()
+        return res.json(error)
+}
+});
+app.put('/products/:id',async function(req,res){
+     const conn = await db.getConnection()
+     try {
+          const checker = await db.executeQuery(conn,`select * from product where id = ${req.params.id}`)
+          if(checker.length ==0){
+               return res.status(404).json({message : 'Data tidak ditemukan'})
+          }
+          const result = await db.executeQuery(conn,`update product set name='${req.body.name}'  where id=${req.params.id}`)
+          const data = await db.executeQuery(conn,`select * from product where id = ${req.params.id}`)
+          conn.release()
+          if(result && data.length >0){
+               return res.json({message : 'Success Update', data : data[0]}) 
+         }
+    } catch (error) {
+        conn.release()
+        return res.json(error)
+}
+});
+app.delete('/products/:id',async function(req,res){
+     const conn = await db.getConnection()
+     try {
+          const checker = await db.executeQuery(conn,`select * from product where id = ${req.params.id}`)
+          if(checker.length ==0){
+               return res.status(404).json({message : 'Data tidak ditemukan'})
+          }
+          const result = await db.executeQuery(conn,`delete from product where id=${req.params.id}`)
+          conn.release()
+          if(result){
+               return res.json('Delete berhasil') 
+         }
+    } catch (error) {
+        conn.release()
+        return res.json(error)
+}
+});
+
+
 
 app.listen(3000, function () {
     console.log("listening to port 3000")
